92587a53af6fa1de6fa82cab109f0d38e6277b42
actuaL aka Alex <74632540+actua-L@users.noreply.github.com>
2022-12-16 12:13:12 -0500

    Create dhcp-search.ps1

---
+++ dhcp-search.ps1
@@ -0,0 +1,10 @@
+$dhcpServers = "SERVER1", "SERVER2" #As Many servers as you need/have
+$IPAddress = Read-Host -Prompt "Enter IP"
+
+$results = foreach ($dhcpServer in $dhcpServers) {
+	Write-Host "Searching $dhcpServer..."
+	Get-DHCPServerv4Scope -ComputerName $dhcpServer | ForEach-Object { Get-DHCPServerv4Reservation -ComputerName $dhcpServer -IPAddress $IPAddress -ErrorAction SilentlyContinue
+	}
+}
+
+Write-Output $results | Select IPAddress, ClientId | Select -Unique
