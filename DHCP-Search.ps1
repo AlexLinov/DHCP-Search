5002689e4b89dc0ea4778deb3a90b3637c40b58d
actuaL aka Alex <74632540+actua-L@users.noreply.github.com>
2022-12-20 15:45:01 -0500

    Update DHCP-Search.ps1

--- DHCP-Search.ps1
+++ DHCP-Search.ps1
@@ -7,27 +7,20 @@ param
 Write-Host "Usage: DHCP-Search.ps1 [IP Address]"
 
 $bannerText = "Searching for IP Lease"
-Write-Host "`n"
 Write-Host "`n" -NoNewline
 Write-Host "========================" -ForegroundColor Green
-Write-Host "`n" -NoNewline
 Write-Host " $bannerText" -ForegroundColor Yellow
-Write-Host "`n" -NoNewline
 Write-Host "========================" -ForegroundColor Green
-Write-Host "`n"
 Start-Sleep -Seconds 1
 
 $dhcpServers = "SERVER1", "SERVER2" #As Many servers as you need/have
 
-#This stops once result is found, instead of searching the rest of servers
-#$dhcpServers | ForEach-Object {
-	#Get-DHCPServerv4Reservation -ComputerName $_ -IPAddress $IPAddress -ErrorAction SilentlyContinue
-#} | Select-Object -First 1
-
-$results = foreach ($dhcpServer in $dhcpServers) {
-	Write-Host "Searching $dhcpServer..."
-	Get-DHCPServerv4Scope -ComputerName $dhcpServer | ForEach-Object { Get-DHCPServerv4Reservation -ComputerName $dhcpServer -IPAddress $IPAddress -ErrorAction SilentlyContinue
-	}
+foreach ($dhcpServer in $dhcpServers) {
+	$reservation = Get-DHCPServerv4Reservation -ComputerName $dhcpServer -IPAddress $IPAddress -ErrorAction SilentlyContinue | Select-Object -First 1 | Select IPAddress, ScopeId, ClientId, Name | ft -autosize -wrap
+	if ($reservation) {
+		$reservation
+		break
+	} else {
+		Write-Output "IP Address not found in DHCP Lease"
 }
-
-Write-Output $results | Select IPAddress, ClientId, Name | Select -Unique #prints out IP Address, MAC Address and Hostname
+}
